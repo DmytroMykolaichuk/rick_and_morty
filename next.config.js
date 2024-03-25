d61b2da588243f4781a0c38cc757e381d1adb5c1
DmytroMykolaichuk <dmitriymikolaychuk@gmail.com>
2024-03-25 08:59:02 +0200

    test

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ images: {
     unoptimized: true,
 },
 // assetPrefix: './',
-basePath: "/rick_and_morty"
+basePath: "/rick_and_morty/src"
 };
 module.exports = nextConfig
 // export default nextConfig;
