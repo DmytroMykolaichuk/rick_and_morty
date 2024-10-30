5067bfea3ba82e66307a5608eecd48f99df3b07a
DmytroMykolaichuk <dmitriymikolaychuk@gmail.com>
2024-10-30 19:30:55 +0200

    fix &apos;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,26 +8,26 @@ export default function Home(): React.ReactNode {
 
       <p>
         Rick and Morty is an American adult animated science fiction sitcom
-        created by Justin Roiland and Dan Harmon for Cartoon Network's nighttime
-        programming block Adult Swim. The series follows the misadventures of
-        Rick Sanchez, a cynical mad scientist, and his good-hearted but fretful
-        grandson Morty Smith, who split their time between domestic life and
-        interdimensional adventures that take place across an infinite number of
-        realities, often traveling to other planets and dimensions through
-        portals and on Rick&apos;s flying saucer. The general concept of Rick
-        and Morty relies on two conflicting scenarios: domestic family drama and
-        a misanthropic grandfather dragging his grandson into hijinks. Roiland
-        voiced both Rick and Morty, with Chris Parnell as Jerry (Rick&apos;s
-        son-in-law and Morty&apos;s father), Spencer Grammer as Summer
-        (Rick&apos;s granddaughter and Morty&apos;s sister), and Sarah Chalke as
-        Beth (Rick&apos;s daughter and Morty&apos;s mother). The series
-        originated from an animated short parody film of Back to the Future
-        created by Roiland for Channel 101, a short-film festival cofounded by
-        Harmon. Since its debut, the series has received critical acclaim for
-        its screenplay, originality, creativity, and humor. It has been
-        nominated for three Primetime Emmy Awards for Outstanding Animated
-        Program and won the award in 2018 and 2020. The series has also received
-        two Annie Awards. At times, the series has been the most viewed
+        created by Justin Roiland and Dan Harmon for Cartoon Network&apos;s
+        nighttime programming block Adult Swim. The series follows the
+        misadventures of Rick Sanchez, a cynical mad scientist, and his
+        good-hearted but fretful grandson Morty Smith, who split their time
+        between domestic life and interdimensional adventures that take place
+        across an infinite number of realities, often traveling to other planets
+        and dimensions through portals and on Rick&apos;s flying saucer. The
+        general concept of Rick and Morty relies on two conflicting scenarios:
+        domestic family drama and a misanthropic grandfather dragging his
+        grandson into hijinks. Roiland voiced both Rick and Morty, with Chris
+        Parnell as Jerry (Rick&apos;s son-in-law and Morty&apos;s father),
+        Spencer Grammer as Summer (Rick&apos;s granddaughter and Morty&apos;s
+        sister), and Sarah Chalke as Beth (Rick&apos;s daughter and Morty&apos;s
+        mother). The series originated from an animated short parody film of
+        Back to the Future created by Roiland for Channel 101, a short-film
+        festival cofounded by Harmon. Since its debut, the series has received
+        critical acclaim for its screenplay, originality, creativity, and humor.
+        It has been nominated for three Primetime Emmy Awards for Outstanding
+        Animated Program and won the award in 2018 and 2020. The series has also
+        received two Annie Awards. At times, the series has been the most viewed
         television comedy for adults between 18 and 24. The popularity of Rick
         and Morty has made it a hundred-million dollar merchandising and media
         franchise. A seventh season was confirmed as part of a long-term deal
